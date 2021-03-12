00797ede198d849c0943fe0a7177670777c28c02
Tuna Fortuna <tuna42na@gmail.com>
2021-03-11 23:37:00 -0500

    CoreJs Fix.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import List from "./components/List";
-import Login from "./components/login";
+import Login from "./components/Login";
 import { Switch, Route } from "react-router-dom";
 import { NameListProvider } from "./components/NameListContext";
 
